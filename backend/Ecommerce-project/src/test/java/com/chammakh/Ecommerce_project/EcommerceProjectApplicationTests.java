9a8175ef09f597900f5bfb53a3d78db88e7dd03e
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2025-07-13 01:30:55 +0000

    Initial commit - Ecommerce-project

---
+++ backend/Ecommerce-project/src/test/java/com/chammakh/Ecommerce_project/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chammakh.Ecommerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
