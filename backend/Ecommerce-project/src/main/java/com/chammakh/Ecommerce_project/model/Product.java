5b3a6161a7bd207cc5063f7f90fba1aaaee9907f
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2025-07-13 16:11:20 +0000

    feat: création de la classe Product pour la gestion des produits

---
+++ backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/model/Product.java
@@ -0,0 +1,31 @@
+package com.chammakh.Ecommerce_project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.aot.AotServices;
+
+import javax.xml.transform.Source;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date release_date;
+    private boolean availability;
+    private int quantity;
+    //private Source image;
+}
