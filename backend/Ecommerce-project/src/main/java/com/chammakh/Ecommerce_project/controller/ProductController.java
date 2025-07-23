6975c2af878d0f01ce7900b71166112e0937406c
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2025-07-23 17:34:34 +0000

    Activation du CORS pour le frontend React

--- backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/controller/ProductController.java
+++ backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/controller/ProductController.java
@@ -4,6 +4,7 @@ package com.chammakh.Ecommerce_project.controller;
 import com.chammakh.Ecommerce_project.model.Product;
 import com.chammakh.Ecommerce_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
