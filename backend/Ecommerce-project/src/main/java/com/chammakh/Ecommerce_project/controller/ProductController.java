144532b8c1856666870e5af24e3447d62e5f41bd
chammAyoub <ayoub.chammakh.23@edu.uiz.ac.ma>
2025-07-29 03:56:26 +0000

    feat: implémentation des fonctionnalités de mise à jour et de suppression des produits
    Ce commit ajoute la possibilité de modifier et supprimer des produits via l'API.
    
    ProductService :
    
    Ajout de la méthode updateProduct pour gérer la mise à jour des produits, y compris la mise à jour du fichier image.
    
    Ajout de la méthode deleteProduct pour supprimer un produit à partir de son identifiant.
    
    ProductController :
    
    Ajout de l’endpoint PUT /product/{id} pour modifier un produit existant.
    
    Ajout de l’endpoint DELETE /product/{id} pour supprimer un produit.

--- backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/controller/ProductController.java
+++ backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -50,6 +51,34 @@ public class ProductController {
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(product.getImageType()))
                 .body(imageFile);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product,
+                                                @RequestPart MultipartFile imageFile){
+        Product product1 = null;
+        try {
+            product1 = service.updateProduct(id, product, imageFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(product1 != null){
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Fauled to update", HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product1 = service.getProductById(id);
+        if(product1 != null) {
+            service.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("Product Not Found", HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/service/ProductService.java
+++ backend/Ecommerce-project/src/main/java/com/chammakh/Ecommerce_project/service/ProductService.java
@@ -4,6 +4,7 @@ import com.chammakh.Ecommerce_project.model.Product;
 import com.chammakh.Ecommerce_project.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -31,5 +32,17 @@ public class ProductService {
         product.setImageData(imageFile.getBytes());
 
         return repository.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return repository.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repository.deleteById(id);
     }
 }
